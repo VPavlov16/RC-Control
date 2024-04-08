6e0d1750606c6a9108b41755a4173af778f59963
Venelin Pavlov <boromazniq@gmail.com>
2024-04-08 09:37:29 +0300

    glove code

---
+++ ArduinoMicro/glove/glove.ino
@@ -0,0 +1,36 @@
+// C++ code
+//
+const int middle = A2;//lilav
+const int pointer = A1;//siv
+const int thumb = A0;//bql
+
+int valueMiddle;
+int valuePointer;
+int valueThumb;
+int threshold = 90;
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  valueMiddle = analogRead(middle);
+  valuePointer = analogRead(pointer);
+  valueThumb = analogRead(thumb);
+  
+ 
+  if (valueMiddle < threshold) {
+    Serial.println("Middle finger is bent!");
+  }
+
+  if (valuePointer < threshold) {
+    Serial.println("Pointer finger is bent!");
+  }
+
+  if (valueThumb < threshold) {
+    Serial.println("Thumb is bent!");
+  }
+
+  delay(1000);
+}
